05c567388873fd15405f6d2afaa447578afbc584
John Morsley <johnmorsley@gmail.com>
2019-10-17 15:04:16 +0100

    Added Swagger and API versioning.

--- Source/Presentation/Morsley.UK.YearPlanner.Users.API/StartUp.cs
+++ Source/Presentation/Morsley.UK.YearPlanner.Users.API/StartUp.cs
@@ -1,40 +1,115 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using Morsley.UK.YearPlanner.Users.API.Swagger;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using SwaggerOptions = Swashbuckle.AspNetCore.Swagger.SwaggerOptions;
 
 namespace Morsley.UK.YearPlanner.Users.API
 {
     public class StartUp
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public IConfiguration Configuration { get; }
+
+        public StartUp(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        // Dependency Injection...
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
+
+            AddApiVersioning(services);
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        // Pipeline...
+        public void Configure(
+            IApplicationBuilder applicationBuilder,
+            IWebHostEnvironment hostingEnvironment,
+            IApiVersionDescriptionProvider apiVersionDescriptionProvider)
         {
-            if (env.IsDevelopment())
+            if (hostingEnvironment.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+                applicationBuilder.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting();
+            applicationBuilder.UseHttpsRedirection();
 
-            app.UseEndpoints(endpoints =>
+            applicationBuilder.UseRouting();
+
+            applicationBuilder.UseAuthorization();
+
+            applicationBuilder.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
+            });
+
+            ConfigureApiVersioning(applicationBuilder, hostingEnvironment, apiVersionDescriptionProvider);
+        }
+
+        #region Private Methods
+
+        private void AddApiVersioning(IServiceCollection services)
+        {
+            var swaggerOptions = new SwaggerOptions();
+            Configuration.GetSection(nameof(SwaggerOptions)).Bind(swaggerOptions);
+
+            services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
+
+            services.AddSwaggerGen(options =>
+            {
+                options.OperationFilter<SwaggerDefaultValues>();
+            });
+
+            services.AddApiVersioning(options =>
+            {
+                options.AssumeDefaultVersionWhenUnspecified = true;
+                options.DefaultApiVersion = ApiVersion.Default;
+                options.ReportApiVersions = true;
+            });
+
+            services.AddVersionedApiExplorer(options =>
+            {
+                options.GroupNameFormat = "'v'VVV";
+                options.SubstituteApiVersionInUrl = true;
+            });
+
+            services.ConfigureSwaggerGen(options =>
+            {
+                options.CustomSchemaIds(x => x.FullName);
             });
         }
+
+        private void ConfigureApiVersioning(
+            IApplicationBuilder applicationBuilder,
+            IWebHostEnvironment hostingEnvironment,
+            IApiVersionDescriptionProvider apiVersionDescriptionProvider)
+        {
+            var swaggerOptions = new SwaggerOptions();
+            Configuration.GetSection(nameof(SwaggerOptions)).Bind(swaggerOptions);
+
+            applicationBuilder.UseSwagger();
+
+            applicationBuilder.UseSwaggerUI(options =>
+            {
+                options.DefaultModelExpandDepth(0);
+                options.DefaultModelsExpandDepth(0);
+                foreach (var description in apiVersionDescriptionProvider.ApiVersionDescriptions)
+                {
+                    options.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json", description.GroupName.ToUpperInvariant());
+                }
+                options.RoutePrefix = "";
+            });
+        }
+
+
+        #endregion Private Methods
     }
 }
---
+++ Source/Presentation/Morsley.UK.YearPlanner.Users.API/Swagger/ConfigureSwaggerOptions.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace Morsley.UK.YearPlanner.Users.API.Swagger
+{
+    public class ConfigureSwaggerOptions : IConfigureOptions<SwaggerGenOptions>
+    {
+        private readonly IApiVersionDescriptionProvider _provider;
+
+        public ConfigureSwaggerOptions(IApiVersionDescriptionProvider provider) => _provider = provider;
+
+        public void Configure(SwaggerGenOptions options)
+        {
+            foreach (var description in _provider.ApiVersionDescriptions)
+            {
+                options.SwaggerDoc(description.GroupName, CreateInfoForApiVersion(description));
+                var xmlCommentsFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlCommentsFullPath = Path.Combine(AppContext.BaseDirectory, xmlCommentsFile);
+                options.IncludeXmlComments(xmlCommentsFullPath);
+            }
+        }
+
+        static OpenApiInfo CreateInfoForApiVersion(ApiVersionDescription description)
+        {
+            var info = new OpenApiInfo
+            {
+                Title = "Morsley UK - Year Planner - Users API",
+                Version = description.ApiVersion.ToString(),
+                Description = "The Users API for Year Planner.",
+                Contact = new OpenApiContact
+                {
+                    Name = "John Morsley",
+                    Email = "john@morsley.uk"
+                },
+                TermsOfService = new Uri("https://morsley.uk/terms-of-service"),
+                License = new OpenApiLicense { Name = "Meh", Url = new Uri("https://morsley.uk/license") }
+            };
+
+            if (description.IsDeprecated)
+            {
+                info.Description += " This API version has been deprecated.";
+            }
+
+            return info;
+        }
+    }
+}
